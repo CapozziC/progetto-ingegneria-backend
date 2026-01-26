772c97dfa34f7200b98ee0a16ceb82ca38052077
CapozziC <153746125+CapozziC@users.noreply.github.com>
2026-01-26 16:48:08 +0100

    Update entity imports to include .js extension and refactor DataSource initialization

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entities/user";
-import { Agent } from "./entities/agent";
-import { Advertisement } from "./entities/advertisement";
-import { Agency } from "./entities/agency";
-import { Appointment } from "./entities/appointment";
-import { Offer } from "./entities/offer";
-import { Photo } from "./entities/photo";
-import { RealEstate } from "./entities/realEstate";
-import { Token } from "./entities/token";
+import { User } from "./entities/user.js";
+import { Agent } from "./entities/agent.js";
+import { Advertisement } from "./entities/advertisement.js";
+import { Agency } from "./entities/agency.js";
+import { Appointment } from "./entities/appointment.js";
+import { Offer } from "./entities/offer.js";
+import { Photo } from "./entities/photo.js";
+import { RealEstate } from "./entities/realEstate.js";
+import { Token } from "./entities/token.js";
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
@@ -17,9 +18,25 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  entities: [Advertisement, Agent, Agency, Appointment, Offer, Photo, RealEstate,Token, User],
+  entities: [
+    Advertisement,
+    Agent,
+    Agency,
+    Appointment,
+    Offer,
+    Photo,
+    RealEstate,
+    Token,
+    User,
+  ],
   migrations: ["db/migrations/*.ts"],
 
   synchronize: false,
   logging: false,
 });
+
+try {
+  await AppDataSource.initialize();
+} catch (error) {
+  console.log(error);
+}
