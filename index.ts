368c2056de031d40c28480fcdb648761a12bf1a2
Ivan Erricis <ivanerricis@libero.it>
2026-01-31 17:59:53 +0100

    Feat: Set up Express server with basic routing

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-import { AppDataSource } from "./db/data-source.js";
+import express from 'express';
+import { AppDataSource } from './db/data-source.js';
 
 try {
   await AppDataSource.initialize();
+  const app = express();
+  const port = 3000;
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 } catch (error) {
   console.log(error);
 }
