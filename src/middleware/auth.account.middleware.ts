d5854ef22797da8c0b31c9c556aa38fc9968e935
CapozziC <153746125+CapozziC@users.noreply.github.com>
2026-03-07 17:30:44 +0100

    fix: comment out error response for expired access token in authentication middleware

--- src/middleware/auth.account.middleware.ts
+++ src/middleware/auth.account.middleware.ts
@@ -63,7 +63,7 @@ export const authenticationMiddlewareAccount = async (
       }
       if (err instanceof ExpiredTokenError) {
         res.clearCookie("accessToken");
-        return res.status(401).json({ error: "Access token expired" });
+        //return res.status(401).json({ error: "Access token expired" });
         // se scaduto -> continuo al refresh flow
       }
     }
