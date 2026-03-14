401535d4b0be2709204c029fddd91994f8be40b9
CapozziC <153746125+CapozziC@users.noreply.github.com>
2026-03-14 15:03:35 +0100

    fix: enhance nodemailer configuration logging and enable SMTP verification

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -1,4 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import nodemailer from "nodemailer";
+console.log("Configuring nodemailer with:", {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  user: process.env.EMAIL_USER,
+  password: process.env.EMAIL_PASSWORD,
+});
 
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
@@ -10,11 +18,12 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-transporter
+/*transporter
   .verify()
   .then(() => console.log("SMTP ok"))
-  .catch((err) => console.error("SMTP non disponibile:", err));
-/*async function testMail() {
+  .catch((err) => console.error("SMTP non disponibile:", err));*/
+
+async function testMail() {
   try {
     await transporter.verify();
     console.log("SMTP ok");
@@ -32,4 +41,4 @@ transporter
   }
 }
 
-testMail();*/
+testMail();
