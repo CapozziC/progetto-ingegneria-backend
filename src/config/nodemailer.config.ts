27ad2cf12d977f4a030fbd4d0a4b2537c410ac5f
CapozziC <153746125+CapozziC@users.noreply.github.com>
2026-03-14 08:21:18 +0100

    fix: change nodemailer transport security setting to true

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT || 587),
-  secure: false, // usa STARTTLS
+  secure: true, // usa STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
