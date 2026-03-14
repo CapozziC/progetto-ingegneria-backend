e4be3acef5070f58e42ba4e764a1a93804489f63
CapozziC <153746125+CapozziC@users.noreply.github.com>
2026-03-14 14:41:39 +0100

    fix: remove default port fallback and add SMTP verification test

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT || 587),
+  port: Number(process.env.EMAIL_PORT),
   secure: true, // usa STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
@@ -10,7 +10,26 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-transporter
+/*transporter
   .verify()
   .then(() => console.log("SMTP ok"))
-  .catch((err) => console.error("SMTP non disponibile:", err));
+  .catch((err) => console.error("SMTP non disponibile:", err));*/
+async function testMail() {
+  try {
+    await transporter.verify();
+    console.log("SMTP ok");
+
+    await transporter.sendMail({
+      from: process.env.MAIL_FROM,
+      to: "fibroga45@gmail.com",
+      subject: "Test SMTP",
+      text: "Test riuscito",
+    });
+
+    console.log("Email inviata");
+  } catch (err) {
+    console.error("Errore SMTP:", err);
+  }
+}
+
+testMail();
